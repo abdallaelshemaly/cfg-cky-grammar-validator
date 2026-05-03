8cc85f1abc8c4e3db7a61d521aacb67b127f6739
abdallaelshemaly <abdallaelshemalyy@gmail.com>
2026-05-03 15:51:42 +0300

    chore: export current parser and grammar helpers

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,30 @@
-from .cky_parser import ParseResult, parse_sentence, parse_tokens
+from .cky_parser import CKYParser, ParseResult, parse_sentence, parse_tokens
+from .evaluator import evaluate_dataset
 from .explainer import ValidationExplanation, explain_result, explain_sentence
-from .grammar import CFGGrammar, get_default_grammar
-from .parse_tree import ParseTree, build_parse_tree, render_parse_tree
+from .grammar import ALLOWED_WORDS, CNF_GRAMMAR, CFGGrammar, get_default_grammar
+from .parse_tree import (
+    ParseTree,
+    build_parse_tree,
+    build_tree,
+    format_tree,
+    render_parse_tree,
+)
 from .tokenizer import tokenize
 
 __all__ = [
+    "ALLOWED_WORDS",
+    "CKYParser",
     "CFGGrammar",
+    "CNF_GRAMMAR",
     "ParseResult",
     "ParseTree",
     "ValidationExplanation",
     "build_parse_tree",
+    "build_tree",
+    "evaluate_dataset",
     "explain_result",
     "explain_sentence",
+    "format_tree",
     "get_default_grammar",
     "parse_sentence",
     "parse_tokens",
